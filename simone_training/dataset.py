f6471f2016d7774b08472f3bea8f5e46c5dec14e
hyperfraise <damien.menigaux@polytechnique.edu>
2025-01-05 11:30:25 +0100

    ss

--- simone_training/dataset.py
+++ simone_training/dataset.py
@@ -318,7 +318,7 @@ class TrackDataset(Dataset):
             )
             tracks_data.append(
                 (
-                    cropped_track_info["vertices"][:, ::120],
+                    cropped_track_info["vertices"],  # [:, ::120],
                     torch.from_numpy(np.array(hands_regions)),
                 )
             )
