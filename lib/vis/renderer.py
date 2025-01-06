bf988589b3c8fa4a364292984e1fb914b99d1b18
hyperfraise <damien.menigaux@polytechnique.edu>
2025-01-06 18:39:49 +0100

    s

--- lib/vis/renderer.py
+++ lib/vis/renderer.py
@@ -460,7 +460,7 @@ def transform_vertices_to_neutral(verts_list, R, T):
     transformed_verts_list = []
     for verts in verts_list:
         # Apply rotation and translation to bring vertices into the neutral camera space
-        verts_transformed = torch.einsum("bij,bvj->bvi", R, verts - T[:, None, :])
+        verts_transformed = torch.einsum("bij,bvj->bvi", -R, verts - T[:, None, :])
         # verts_transformed[..., 1] = -verts_transformed[..., 1]
         transformed_verts_list.append(verts_transformed)
     return transformed_verts_list
