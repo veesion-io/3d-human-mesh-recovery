6f1b90a33ed1c5ae5475feea27c839b07d8d929e
hyperfraise <damien.menigaux@polytechnique.edu>
2025-01-06 13:54:16 +0100

    s

--- simone_training/dataset.py
+++ simone_training/dataset.py
@@ -48,7 +48,7 @@ def find_closest(sorted_list1, sorted_list2):
     indices = np.abs(sorted_list1[:, None] - sorted_list2).argmin(axis=1)
 
     # Use the indices to get the closest values from sorted_list2
-    return sorted_list2[indices], indices
+    return sorted_list2[indices]
 
 
 import cv2
@@ -59,7 +59,8 @@ class TrackDataset(Dataset):
         self,
         meta_data_file,
         window_duration,
-        tracks_path="results",
+        tracks_path="tracks",
+        images_path="results",
         target_fps=3.0,
         hands_height=128,
         hands_width=128,
@@ -78,6 +79,7 @@ class TrackDataset(Dataset):
         self.videos_meta_data = {k: self.videos_meta_data[k] for k in self.videos_names}
         self.window_duration = window_duration
         self.tracks_path = tracks_path
+        self.images_path = images_path
         self.target_fps = target_fps
         self.hands_height = hands_height
         self.hands_width = hands_width
@@ -85,177 +87,48 @@ class TrackDataset(Dataset):
     def __len__(self):
         return len(self.videos_meta_data)
 
+    # from collections import defaultdict
+
     def load_video_tracks(
         self,
         video_name,
-        height,
-        width,
-        bin_size=-1,
-        floor_scale=2,
-        max_faces_per_bin=30000,
     ):
-        video_barename = os.path.splitext(video_name)[0].split(".")[0]
-        img_folder = os.path.join(self.tracks_path, video_barename, "images")
-        hps_folder = os.path.join(self.tracks_path, video_barename, "hps")
-        imgfiles = sorted(glob(f"{img_folder}/*.jpg"))
-        hps_files = sorted(glob(f"{hps_folder}/*.npy"))
-        camera_file = os.path.join(self.tracks_path, video_barename, "camera.npy")
-        smpl = SMPL()
-        colors = np.loadtxt("data/colors.txt") / 255
-        colors = torch.from_numpy(colors).float()
+        video_barename = os.path.splitext(video_name)[0]
+        tracks_info = np.load(
+            os.path.join(self.tracks_path, f"{video_barename}.npy", allow_pickle=True)
+        ).item()
+        return tracks_info
+        # formatted_tracks = {track_id : {"frames_ids": frames_ids} for track_id, frames_ids in tracks_frames_ids.items()}
 
-        max_track = len(hps_files)
-        track_verts = {i: [] for i in range(len(imgfiles))}
-        track_joints = {i: [] for i in range(len(imgfiles))}
-        track_tid = {i: [] for i in range(len(imgfiles))}
-
-        ##### TRAM + VIMO #####
-        pred_cam = np.load(camera_file, allow_pickle=True).item()
-        img_focal = pred_cam["img_focal"].item()
-        pred_cam_R = torch.tensor(pred_cam["pred_cam_R"])
-        pred_cam_T = torch.tensor(pred_cam["pred_cam_T"])
-        locations = []
-        tracks_info = {}
-        for i in range(max_track):
-            hps_file = hps_files[i]
-
-            pred_smpl = np.load(hps_file, allow_pickle=True).item()
-            pred_rotmat = pred_smpl["pred_rotmat"]
-            pred_shape = pred_smpl["pred_shape"]
-            pred_trans = pred_smpl["pred_trans"]
-            frame = pred_smpl["frame"]
-
-            mean_shape = pred_shape.mean(dim=0, keepdim=True)
-            pred_shape = mean_shape.repeat(len(pred_shape), 1)
-
-            pred = smpl(
-                body_pose=pred_rotmat[:, 1:],
-                global_orient=pred_rotmat[:, [0]],
-                betas=pred_shape,
-                transl=pred_trans.squeeze(),
-                pose2rot=False,
-                default_smpl=True,
-            )
-            pred_vert = pred.vertices
-            pred_j3d = pred.joints[:, :24]
-
-            cam_r = pred_cam_R[frame]
-            cam_t = pred_cam_T[frame]
-
-            pred_vert_w = (
-                torch.einsum("bij,bnj->bni", cam_r, pred_vert) + cam_t[:, None]
-            )
-            pred_j3d_w = torch.einsum("bij,bnj->bni", cam_r, pred_j3d) + cam_t[:, None]
-            pred_vert_w, pred_j3d_w = traj_filter(pred_vert_w, pred_j3d_w)
-            locations.append(pred_j3d_w.mean(1))
-
-            for j, f in enumerate(frame.tolist()):
-                track_tid[f].append(i)
-                track_verts[f].append(pred_vert_w[j])
-                track_joints[f].append(pred_j3d_w[j])
-            tracks_info[i] = {"vertices": pred_vert_w, "frames_ids": frame}
-        ##### Fit to Ground #####
-        grounding_verts = []
-        grounding_joints = []
-        for t in range(len(imgfiles)):
-            try:
-                grounding_verts.append(torch.stack(track_verts[t]))
-                grounding_joints.append(torch.stack(track_joints[t]))
-            except Exception:
-                continue
-
-        grounding_verts = torch.cat(grounding_verts)
-        grounding_joints = torch.cat(grounding_joints)
-
-        R, offset = fit_to_ground_easy(grounding_verts, grounding_joints)
-        offset = torch.tensor([0, offset, 0])
-        locations = torch.cat(locations)
-        locations = torch.einsum("ij,bj->bi", R, locations) - offset
-        cx, cz = (locations.max(0)[0] + locations.min(0)[0])[[0, 2]] / 2.0
-        sx, sz = (locations.max(0)[0] - locations.min(0)[0])[[0, 2]]
-        scale = max(sx.item(), sz.item()) * floor_scale
-
-        ##### Viewing Camera #####
-        pred_cam = np.load(camera_file, allow_pickle=True).item()
-        pred_cam_R = torch.tensor(pred_cam["pred_cam_R"])
-        pred_cam_T = torch.tensor(pred_cam["pred_cam_T"])
-
-        cam_R = torch.einsum("ij,bjk->bik", R, pred_cam_R)
-        cam_T = torch.einsum("ij,bj->bi", R, pred_cam_T) - offset
-        cam_R = cam_R.mT
-        cam_T = -torch.einsum("bij,bj->bi", cam_R, cam_T)
-
-        cam_R = cam_R.to("cuda")
-        cam_T = cam_T.to("cuda")
-        renderer = Renderer(
-            width,
-            height,
-            img_focal,
-            "cuda",
-            smpl.faces,
-            bin_size=bin_size,
-            max_faces_per_bin=max_faces_per_bin,
-        )
-        renderer.set_ground(scale, cx.item(), cz.item())
-        cameras, _ = renderer.create_camera_from_cv(cam_R[[0]], cam_T[[0]])
-        return cameras, tracks_info, offset, R
-
-    def crop_track(self, track_info, video_fps, target_fps, timespan):
+    def crop_track(self, track_id, video_tracks, video_fps, target_fps, timespan):
         step = 1 / target_fps
         timestamps_to_select = np.arange(timespan[0], timespan[1] + step / 2, step)
         frames_ids_to_select = timestamps_to_select * video_fps
-        adjusted_frames_ids, indices = find_closest(
-            frames_ids_to_select, track_info["frames_ids"]
+        adjusted_frames_ids = find_closest(
+            frames_ids_to_select, video_tracks["frames_ids"][track_id]
         )
         return {
             "frames_ids": adjusted_frames_ids,
-            "vertices": track_info["vertices"][indices],
+            "vertices": [
+                video_tracks["vertices"][frame_id][track_id]
+                for frame_id in adjusted_frames_ids
+            ],
         }
 
     def load_hands_regions(
-        self, video_name, video_camera, track_info, camera_offset, camera_R
+        self, track_id, video_name, video_tracks, cropped_track_info
     ):
-        video_barename = os.path.splitext(video_name)[0].split(".")[0]
-        img_folder = os.path.join(self.tracks_path, video_barename, "images")
+        video_barename = os.path.splitext(video_name)[0]
+        img_folder = os.path.join(
+            self.images_path, video_barename.split(".")[0], "images"
+        )
         imgfiles = sorted(glob(f"{img_folder}/*.jpg"))
         track_hands = []
-        for frame_id, frame_vertices in zip(
-            track_info["frames_ids"], track_info["vertices"]
-        ):
+        for frame_id in cropped_track_info["frames_ids"]:
             img = np.ascontiguousarray(
                 cv2.imread(imgfiles[frame_id])[:, :, ::-1], dtype=np.uint8
             )
-            reshaped_vertices = frame_vertices.unsqueeze(0)  # [:,None]
-            reshaped_vertices = torch.einsum(
-                "ij,bnj->bni", camera_R, reshaped_vertices
-            )[:, None]
-            reshaped_vertices -= camera_offset
-            reshaped_vertices = reshaped_vertices.to("cuda")
-
-            hands_points = reshaped_vertices[0, 0, [2500, 5500]]
-            screen_points = video_camera.transform_points_screen(
-                hands_points, image_size=img.shape[:2]
-            )
-
-            # Extract screen coordinates
-            x_coords = screen_points[..., 0]
-            y_coords = screen_points[..., 1]
-
-            # Convert to image indices (integer pixel indices)
-            person_hands = (
-                torch.stack([y_coords, x_coords], dim=-1).long().data.cpu().numpy()
-            )
-            extremal_points = reshaped_vertices[0, 0, [0, 5000]]
-            # Project points to the screen space
-            screen_points = video_camera.transform_points_screen(
-                extremal_points, image_size=img.shape[:2]
-            )
-
-            # Extract screen coordinates
-            x_coords = screen_points[..., 0]
-            y_coords = screen_points[..., 1]
-            points = torch.stack([y_coords, x_coords], dim=-1).long().data.cpu().numpy()
-            height = np.sum((points[0, :] - points[1, :]) ** 2) ** 0.5
+            person_hands, height = video_tracks["hands"][frame_id][track_id]
             frame_hands_regions = []
             for x, y in person_hands:
                 dx = int(0.21 * height)
@@ -277,10 +150,10 @@ class TrackDataset(Dataset):
             track_hands.append(frame_hands_regions)
         return track_hands
 
-    def track_in_window(self, video_fps, track_info, window):
+    def track_in_window(self, video_fps, frames_ids, window):
         track_timespan = [
-            track_info["frames_ids"][0] / video_fps,
-            track_info["frames_ids"][-1] / video_fps,
+            frames_ids[0] / video_fps,
+            frames_ids[-1] / video_fps,
         ]
         return compute_timestamp_intersection(window, track_timespan) > 0.2
 
@@ -294,27 +167,24 @@ class TrackDataset(Dataset):
         video_info = read_video_info(os.path.join("simone_bag_subset", video_name))
         # Check if metadata is missing or invalid
         try:
-            video_camera, video_tracks, camera_offset, camera_R = (
-                self.load_video_tracks(
-                    video_name, video_info["height"], video_info["width"]
-                )
-            )
-        except:
+            video_tracks = self.load_video_tracks(video_name)
+        except FileNotFoundError:
             return None
         tracks_data = []
-        for track_id, track_info in video_tracks.items():
+        for track_id, frames_ids in video_tracks["frames_ids"].items():
             if not self.track_in_window(
-                video_info["fps"], track_info, [start_time, end_time]
+                video_info["fps"], frames_ids, [start_time, end_time]
             ):
                 continue
             cropped_track_info = self.crop_track(
-                track_info,
+                track_id,
+                video_tracks,
                 video_info["fps"],
                 self.target_fps,
                 [start_time, end_time],
             )
             hands_regions = self.load_hands_regions(
-                video_name, video_camera, cropped_track_info, camera_offset, camera_R
+                track_id, video_name, video_tracks, cropped_track_info
             )
             tracks_data.append(
                 (
