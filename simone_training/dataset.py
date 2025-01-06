61f12fa5b427d9920f3156047f9b40afe5a70b8f
hyperfraise <damien.menigaux@polytechnique.edu>
2025-01-06 14:20:23 +0100

    s

--- simone_training/dataset.py
+++ simone_training/dataset.py
@@ -216,7 +216,7 @@ class TrackDataset(Dataset):
             "label": label,
         }
         os.makedirs("inputs", exist_ok=True)
-        np.save(f"inputs/{os.path.splitext(video_barename)[0]}.npy", formatted_data)
+        np.save(f"inputs/{os.path.splitext(video_name)[0]}.npy", formatted_data)
         # np.save("hands.npy", formatted_data["hands_regions"].numpy())
         # print(video_name, start_time)
         # dvsdv
