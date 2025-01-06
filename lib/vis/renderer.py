2801e16ae1b002e271808a9cb9b4da5baf590cc3
hyperfraise <damien.menigaux@polytechnique.edu>
2025-01-06 18:33:55 +0100

    s

--- lib/vis/renderer.py
+++ lib/vis/renderer.py
@@ -463,7 +463,7 @@ def transform_vertices_to_neutral(verts_list, R, T):
         verts_transformed = torch.einsum(
             "bij,bvj->bvi", R.transpose(1, 2), verts - T[:, None, :]
         )
-        verts_transformed[..., 0] = -verts_transformed[..., 0]
+        verts_transformed[..., 2] = -verts_transformed[..., 2]
         transformed_verts_list.append(verts_transformed)
     return transformed_verts_list
 
